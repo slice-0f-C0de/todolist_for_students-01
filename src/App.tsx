7dddd50c546b85c7edeec809928621dc28afd688
slice_of_code <slice_of_code@mail.ru>
2023-02-06 17:40:53 +0400

    01

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import {Todolist, TodolistTasksType} from "./Todolist";
 function App() {
 
     let task1: Array<TodolistTasksType> = [ // Массивы по желанию тоже можно типизировать
-        {id:1,title: "HTML",isDone: true},
-        {id:1,title: "CSS",isDone: true},
-        {id:1,title: "JavaScript",isDone: false}
+        {id:1, title: "HTML", isDone: true},
+        {id:2, title: "CSS", isDone: true},
+        {id:3, title: "JavaScript", isDone: false}
     ]
 
     let task2: Array<TodolistTasksType> = [
-        {id:1,title: "Pulp Fiction",isDone: true},
-        {id:1,title: "Kill Bill vol. 1/2",isDone: true},
-        {id:1,title: "Once Upon a Time... in Hollywood",isDone: true}
+        {id:1, title: "Pulp Fiction", isDone: true},
+        {id:2, title: "Kill Bill vol. 1/2", isDone: true},
+        {id:3, title: "Once Upon a Time... in Hollywood", isDone: true}
     ] // 1. Создаём два массива
 
     return (
